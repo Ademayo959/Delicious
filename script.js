8af4ddf3f15fc3568ae3e181904d593d31e1ea92
Oluwamayowa <118119872+Ademayo959@users.noreply.github.com>
2025-05-19 10:06:30 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const phoneNavBtn = document.querySelector('.phone-nav-btn');
-const phoneNavbar = document.querySelector('.phone-navbar')
-phoneNavBtn.addEventListener('click', () => {
-    if (phoneNavbar.style.display === 'block') {
-        phoneNavbar.style.display = 'none';
-    } else {
-        phoneNavbar.style.display = 'block';
-    }
-});
